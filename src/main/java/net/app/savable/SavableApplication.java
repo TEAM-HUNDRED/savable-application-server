cad77dbfd3f62682b327d8707a6f5db36f7fb9ef
DAY0522 <ekdud_____@naver.com>
2023-08-08 22:25:58 +0900

    initial spring boot

---
+++ src/main/java/net/app/savable/SavableApplication.java
@@ -0,0 +1,13 @@
+package net.app.savable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/app/savable/SavableApplicationTests.java
@@ -0,0 +1,13 @@
+package net.app.savable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
