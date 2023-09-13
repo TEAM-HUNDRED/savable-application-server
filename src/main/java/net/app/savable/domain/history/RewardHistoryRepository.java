d1fd6d4f0de90c901d59abf5809937719ea40f40
DAY0522 <ekdud_____@naver.com>
2023-09-14 05:03:32 +0900

    feat: RewardHistoryRepository 구현

---
+++ src/main/java/net/app/savable/domain/history/RewardHistoryRepository.java
@@ -0,0 +1,6 @@
+package net.app.savable.domain.history;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RewardHistoryRepository extends JpaRepository<RewardHistory, Long>{
+}
