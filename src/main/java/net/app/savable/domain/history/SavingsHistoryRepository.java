a7478fab302b8a71855f6709fc95536c3854fe5f
DAY0522 <ekdud_____@naver.com>
2023-09-14 05:04:13 +0900

    feat: SavingsHistoryRepository 구현

---
+++ src/main/java/net/app/savable/domain/history/SavingsHistoryRepository.java
@@ -0,0 +1,6 @@
+package net.app.savable.domain.history;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SavingsHistoryRepository extends JpaRepository<SavingsHistory, Long>
+}
