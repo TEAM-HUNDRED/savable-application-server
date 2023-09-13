11ef3745f98e2de246ec3dfb85accd675334b1cf
DAY0522 <ekdud_____@naver.com>
2023-09-14 02:42:15 +0900

    feat: challenge, challenge_verification_guide domain 코드 구현
    - 연관 관계 매핑 진행

---
+++ src/main/java/net/app/savable/domain/challenge/Challenge.java
@@ -0,0 +1,42 @@
+package net.app.savable.domain.challenge;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+public class Challenge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String image; // 챌린지 썸네일 이미지
+
+    @Column(nullable = false)
+    private String explanation; // 챌린지 설명
+
+    @Column(nullable = false)
+    private String verificationDescription; // 인증방법 설명
+
+    @Column(nullable = false)
+    private Long estimatedSavings; // 1회 인증당 절약추정금액
+
+    @Column(nullable = false)
+    private Long reward; // 1회 인증당 리워드
+
+    @Column(nullable = false)
+    private Boolean hasDeadline; // 마감 여부 (false: 상시모집, true: 기간모집)
+
+    private LocalDate startDate; // 챌린지 모집 시작일
+    private LocalDate endDate; // 챌린지 모잡 마감일
+
+    @OneToMany(mappedBy = "challenge", cascade = CascadeType.ALL) // Challenge 1 : N ChallengeVerificationGuide
+    private List<ChallengeVerificationGuide> challengeVerificationGuide;
+}
