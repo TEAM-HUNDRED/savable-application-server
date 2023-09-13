5949d5b50246c8379320098f56c103319d10c630
DAY0522 <ekdud_____@naver.com>
2023-09-14 02:53:15 +0900

    feat: participation_challenge column nullable 속성 추가

--- src/main/java/net/app/savable/domain/challenge/ParticipationChallenge.java
+++ src/main/java/net/app/savable/domain/challenge/ParticipationChallenge.java
@@ -13,10 +13,19 @@ public class ParticipationChallenge {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private LocalDate startDate; // 챌린지 참여 시작일
+
+    @Column(nullable = false)
     private LocalDate endDate; // 챌린지 참여 마감일
+
+    @Column(nullable = false)
     private Long verificationGoal; // 인증 목표 횟수
+
+    @Column(nullable = false)
     private Boolean isSuccess; // 챌린지 성공 여부
+
+    @Column(nullable = false)
     private Long savings; // 챌린지 인증당 절약 금액
 
     @ManyToOne(fetch = FetchType.LAZY) // ParticipationChallenge N : 1 Challenge (지연로딩)
