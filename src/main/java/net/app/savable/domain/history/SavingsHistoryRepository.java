4d6ef4ac2b7897946be27b23714df6bfd1a9fc9d
DAY0522 <ekdud_____@naver.com>
2023-09-14 05:04:30 +0900

    fix: 중괄호 빠진 현상 수정

--- src/main/java/net/app/savable/domain/history/SavingsHistoryRepository.java
+++ src/main/java/net/app/savable/domain/history/SavingsHistoryRepository.java
@@ -2,5 +2,5 @@ package net.app.savable.domain.history;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SavingsHistoryRepository extends JpaRepository<SavingsHistory, Long>
+public interface SavingsHistoryRepository extends JpaRepository<SavingsHistory, Long> {
 }
