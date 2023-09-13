ab2c42ed4b7696296c60a65d989d18d342a07f08
DAY0522 <ekdud_____@naver.com>
2023-09-14 02:46:39 +0900

    feat: participation_challenge domain 코드 작성

---
+++ src/main/java/net/app/savable/domain/challenge/ParticipationChallenge.java
@@ -0,0 +1,20 @@
+package net.app.savable.domain.challenge;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+public class ParticipationChallenge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate startDate; // 챌린지 참여 시작일
+    private LocalDate endDate; // 챌린지 참여 마감일
+    private Long verificationGoal; // 인증 목표 횟수
+    private Boolean isSuccess; // 챌린지 성공 여부
+    private Long savings; // 챌린지 인증당 절약 금액
+}
