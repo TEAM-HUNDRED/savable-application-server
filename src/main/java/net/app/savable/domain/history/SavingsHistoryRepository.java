2fd78e1bb5f0d50608e8fcb8f9985fcb7a820784
DAY0522 <ekdud_____@naver.com>
2023-09-14 05:06:26 +0900

    fix: @Repository 누락 수정

--- src/main/java/net/app/savable/domain/history/SavingsHistoryRepository.java
+++ src/main/java/net/app/savable/domain/history/SavingsHistoryRepository.java
@@ -1,6 +1,8 @@
 package net.app.savable.domain.history;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SavingsHistoryRepository extends JpaRepository<SavingsHistory, Long> {
 }
