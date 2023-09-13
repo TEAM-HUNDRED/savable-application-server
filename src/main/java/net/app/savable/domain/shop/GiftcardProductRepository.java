0819ba336df54120e50c51fbe5d0be6f5c2dfeed
DAY0522 <ekdud_____@naver.com>
2023-09-14 05:05:38 +0900

    feat: GiftcardProductRepository 구현

---
+++ src/main/java/net/app/savable/domain/shop/GiftcardProductRepository.java
@@ -0,0 +1,6 @@
+package net.app.savable.domain.shop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GiftcardProductRepository extends JpaRepository<GiftcardProduct, Long> {
+}
