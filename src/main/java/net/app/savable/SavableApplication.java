8f9ec0f14cbed016fdce04d1eb588a264054ab6f
DAY0522 <ekdud_____@naver.com>
2023-09-14 12:03:22 +0900

    feat: JPA Auditing 활성화

--- src/main/java/net/app/savable/SavableApplication.java
+++ src/main/java/net/app/savable/SavableApplication.java
@@ -3,9 +3,12 @@ package net.app.savable;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SavableApplication {
 
 	public static void main(String[] args) {
