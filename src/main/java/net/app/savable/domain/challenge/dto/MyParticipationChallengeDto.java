7d172e8ff5ccf3e20d2c24265b673417b3c70693
DAY0522 <ekdud_____@naver.com>
2023-09-15 20:16:42 +0900

    feat: MyParticipationChallengeDto에 isVerifiedToday 변수 추가
    - 오늘 인증 여부를 알기 위한 isVerifiedToday 변수를 추가함.

--- src/main/java/net/app/savable/domain/challenge/dto/MyParticipationChallengeDto.java
+++ src/main/java/net/app/savable/domain/challenge/dto/MyParticipationChallengeDto.java
@@ -11,8 +11,9 @@ public class MyParticipationChallengeDto {
     private Long percentage;
     private Long scheduledReward;
     private Long savings;
+    private Boolean isVerifiedToday;
 
-    public MyParticipationChallengeDto(Long participationChallengeId, String image, String title, Long remainingVerification, Long percentage, Long scheduledReward, Long savings) {
+    public MyParticipationChallengeDto(Long participationChallengeId, String image, String title, Long remainingVerification, Long percentage, Long scheduledReward, Long savings, Boolean isVerifiedToday) {
         this.participationChallengeId = participationChallengeId;
         this.image = image;
         this.title = title;
@@ -20,5 +21,6 @@ public class MyParticipationChallengeDto {
         this.percentage = percentage;
         this.scheduledReward = scheduledReward;
         this.savings = savings;
+        this.isVerifiedToday = isVerifiedToday;
     }
 }
