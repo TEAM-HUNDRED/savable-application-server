7045441e7495d2ab29618d89f17632b4df9751ce
DAY0522 <ekdud_____@naver.com>
2023-09-19 03:23:09 +0900

    feat: ApiResponse 작성
    - Response Body 출력 시 공통적인 Format 설정을 위한 ApiResponse 개발

---
+++ src/main/java/net/app/savable/global/common/ApiResponse.java
@@ -0,0 +1,46 @@
+package net.app.savable.global.common;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+
+@Slf4j
+@Getter
+public class ApiResponse<T> {
+    private boolean success;
+    private T data;
+    private int code;
+    private HttpStatus status;
+    private String message;
+
+    public static <T> ApiResponse<T> fail(ErrorCode errorCode) {
+        log.info("ApiResponse.fail: {}", errorCode.getMessage());
+        ApiResponse<T> response = new ApiResponse<>();
+        response.success = false;
+        response.code = errorCode.getCode();
+        response.status = errorCode.getHttpStatus();
+        response.message = errorCode.getMessage();
+        return response;
+    }
+
+    public static <T> ApiResponse<T> fail(ErrorCode errorCode, String message) {
+        log.info("ApiResponse.fail: {}", message);
+        ApiResponse<T> response = new ApiResponse<>();
+        response.success = false;
+        response.code = errorCode.getCode();
+        response.status = errorCode.getHttpStatus();
+        response.message = message;
+        return response;
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        log.info("ApiResponse.success: {}", data);
+        ApiResponse<T> response = new ApiResponse<>();
+        response.success = true;
+        response.data = data;
+        response.code = ErrorCode.OK.getCode();
+        response.status = ErrorCode.OK.getHttpStatus();
+        response.message = ErrorCode.OK.getMessage();
+        return response;
+    }
+}
