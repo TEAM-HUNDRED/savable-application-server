8d50eccd32164e2e01951125dd703c37df73d291
DAY0522 <ekdud_____@naver.com>
2023-09-19 03:31:11 +0900

    feat: GeneralException 구현
    - 공통 에러 처리를 위한 GeneralException 구현
    - @RestControllerAdvice로 에러 발생 시, ErrorCode를 불러오기 위한 코드

---
+++ src/main/java/net/app/savable/global/common/GeneralException.java
@@ -0,0 +1,49 @@
+package net.app.savable.global.common;
+
+import lombok.Getter;
+
+@Getter
+public class GeneralException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public GeneralException() {
+        super(ErrorCode.INTERNAL_SERVER_ERROR.getMessage());
+        this.errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+    }
+
+    public GeneralException(String message) {
+        super(ErrorCode.INTERNAL_SERVER_ERROR.getMessage(message));
+        this.errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+    }
+
+    public GeneralException(String message, Throwable cause) {
+        super(ErrorCode.INTERNAL_SERVER_ERROR.getMessage(message), cause);
+        this.errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+    }
+
+    public GeneralException(Throwable cause) {
+        super(ErrorCode.INTERNAL_SERVER_ERROR.getMessage(cause));
+        this.errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+    }
+
+    public GeneralException(ErrorCode errorErrorcode) {
+        super(errorErrorcode.getMessage());
+        this.errorCode = errorErrorcode;
+    }
+
+    public GeneralException(ErrorCode errorErrorcode, String message) {
+        super(errorErrorcode.getMessage(message));
+        this.errorCode = errorErrorcode;
+    }
+
+    public GeneralException(ErrorCode errorErrorcode, String message, Throwable cause) {
+        super(errorErrorcode.getMessage(message), cause);
+        this.errorCode = errorErrorcode;
+    }
+
+    public GeneralException(ErrorCode errorErrorcode, Throwable cause) {
+        super(errorErrorcode.getMessage(cause), cause);
+        this.errorCode = errorErrorcode;
+    }
+}
