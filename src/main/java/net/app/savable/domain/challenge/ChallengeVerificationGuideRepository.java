a283d7119c1b986ca573b99693bb8671cbd1c77e
chae52 <fjdksla98@naver.com>
2023-09-20 01:44:17 +0900

    feat:statusCode 추가, key값 수정

--- src/main/java/net/app/savable/domain/challenge/ChallengeVerificationGuideRepository.java
+++ src/main/java/net/app/savable/domain/challenge/ChallengeVerificationGuideRepository.java
@@ -1,8 +1,12 @@
 package net.app.savable.domain.challenge;
 
+import net.app.savable.domain.challenge.dto.ChallengeGuideDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ChallengeVerificationGuideRepository extends JpaRepository<ChallengeVerificationGuide, Long> {
+    List<ChallengeVerificationGuide> findByChallengeIdOrderByIsPassDesc(Integer challengeId);
 }
--- src/main/java/net/app/savable/service/ChallengeService.java
+++ src/main/java/net/app/savable/service/ChallengeService.java
@@ -1,2 +1,32 @@
-package net.app.savable.service;public class ChallengeService {
+    package net.app.savable.service;
+
+import lombok.RequiredArgsConstructor;
+import net.app.savable.domain.challenge.Challenge;
+import net.app.savable.domain.challenge.ChallengeRepository;
+import net.app.savable.domain.challenge.ChallengeVerificationGuideRepository;
+import net.app.savable.domain.challenge.dto.ChallengeDto;
+import net.app.savable.domain.challenge.dto.ChallengeGuideDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChallengeService {
+    private final ChallengeVerificationGuideRepository guideRepository;
+    private final ChallengeRepository challengeRepository;
+
+    public ChallengeDto findChallengeById(Integer challengeId){
+        Challenge challengeDetail = challengeRepository.findById(challengeId);
+        ChallengeDto challengeDto = new ChallengeDto(challengeDetail);
+        return challengeDto;
+    }
+
+    public List<ChallengeGuideDto> findChallengeGuide(Integer challengeId){
+        List<ChallengeGuideDto> challengeGuideDtoList = guideRepository.findByChallengeIdOrderByIsPassDesc(challengeId).stream()
+                .map(ChallengeGuideDto::new)
+                .toList();
+        return challengeGuideDtoList;
+    }
+
 }
