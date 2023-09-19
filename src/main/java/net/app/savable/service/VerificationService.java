75fc139e9ae6eb7c127b03f23e278e395e3a8da8
DAY0522 <ekdud_____@naver.com>
2023-09-20 04:01:46 +0900

    feat: VerificationService 구현

---
+++ src/main/java/net/app/savable/service/VerificationService.java
@@ -0,0 +1,23 @@
+package net.app.savable.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.app.savable.domain.challenge.VerificationRepository;
+import net.app.savable.domain.challenge.dto.VerificationRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class VerificationService {
+
+    private final VerificationRepository verificationRepository;
+
+    @Transactional(readOnly = false)
+    public void addVerification(VerificationRequestDto verificationRequestDto) {
+        log.info("addVerification: {}", verificationRequestDto);
+        verificationRepository.save(verificationRequestDto.toEntity());
+    }
+}
