26260956b1cd61e4e60ca577d8b0f381a1d50c84
DAY0522 <ekdud_____@naver.com>
2023-09-21 04:42:58 +0900

    feat: 챌린지 성공 여부 판단 후, 리워드를 지급하는 비즈니스 로직 구현

--- src/main/java/net/app/savable/service/SchedulerService.java
+++ src/main/java/net/app/savable/service/SchedulerService.java
@@ -2,9 +2,9 @@ package net.app.savable.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.app.savable.domain.challenge.Verification;
-import net.app.savable.domain.challenge.VerificationRepository;
-import net.app.savable.domain.challenge.VerificationState;
+import net.app.savable.domain.challenge.*;
+import net.app.savable.domain.member.Member;
+import net.app.savable.domain.member.MemberRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,20 +18,65 @@ import java.util.List;
 public class SchedulerService {
 
     private final VerificationRepository verificationRepository;
+    private final ParticipationChallengeRepository participationRepository;
+    private final MemberRepository memberRepository;
+    private final ChallengeRepository challengeRepository;
 
     @Transactional(readOnly = false)
-    @Scheduled(cron = "0 0 7 * * *") // 매일 7시에 실행
-    public void checkVerification() { // 일정 시간에 인증 상태를 SUCCESS로 변경하는 함수.
+    @Scheduled(cron = "1 0 0 * * *") // 매일 0시 0분 1초에 실행
+    public void checkVerification() {
         log.info("SchedulerService.checkVerification() 실행");
-        List<Verification> verifications = verificationRepository.findByStateIs(VerificationState.WAITING); // 인증 상태가 WAITING인 인증
+        List<Verification> waitingVerifications = verificationRepository.findByStateIs(VerificationState.WAITING);
 
-        if (verifications.isEmpty()) { // 인증이 없으면 종료
+        if (waitingVerifications.isEmpty()) {
             log.warn("No verifications found with WAITING state.");
             return;
         }
 
-        for (Verification verification : verifications) {
-            verification.updateState(VerificationState.SUCCESS); // 인증 상태를 SUCCESS로 변경
+        for (Verification verification : waitingVerifications) {
+            processVerification(verification);
         }
+    }
+
+    private void processVerification(Verification verification) {
+        verification.updateState(VerificationState.SUCCESS);
+
+        Long participationId = verification.getParticipationChallengeId();
+        Long successCount = verificationRepository.countByParticipationChallenge_IdAndState(participationId, VerificationState.SUCCESS);
+
+        ParticipationChallenge participation = participationRepository.findById(participationId)
+                .orElseThrow(() -> {
+                    log.error("Invalid participation ID: {}", participationId);
+                    return new IllegalArgumentException("Invalid participation ID: " + participationId);
+                });
+
+        if (isParticipationSuccessful(participation, successCount)) {
+            rewardSuccessfulParticipation(participation, successCount);
+        }
+    }
+
+    private boolean isParticipationSuccessful(ParticipationChallenge participation, Long successCount) {
+        return successCount >= participation.getVerificationGoal();
+    }
+
+    private void rewardSuccessfulParticipation(ParticipationChallenge participation, Long successCount) {
+        log.info("챌린지 성공!");
+        participation.updateState(ParticipationState.SUCCESS);
+
+        Member member = memberRepository.findById(participation.getMemberId())
+                .orElseThrow(() -> {
+                    log.error("Invalid member ID: {}", participation.getMemberId());
+                    return new IllegalArgumentException("Invalid member ID: " + participation.getMemberId());
+                });
+
+        member.updateSavings(participation.getSavings() * successCount);
+
+        Challenge challenge = challengeRepository.findById(participation.getChallengeId())
+                .orElseThrow(() -> {
+                    log.error("Invalid challenge ID: {}", participation.getChallengeId());
+                    return new IllegalArgumentException("Invalid challenge ID: " + participation.getChallengeId());
+                });
+
+        member.updateReward(challenge.getReward() * successCount);
     }
 }
