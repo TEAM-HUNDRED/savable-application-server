9c0a2500ac76834d7ab652a3d677dd39986619fa
DAY0522 <ekdud_____@naver.com>
2023-09-27 05:09:46 +0900

    fix: JPA 정의 method를 사용하도록 변경
    - 기존에 findMemberById를 사용하는 것과 달리 JPA 기본 함수인 findById를 사용하도록 변경

--- src/main/java/net/app/savable/service/MemberService.java
+++ src/main/java/net/app/savable/service/MemberService.java
@@ -1,17 +1,19 @@
 package net.app.savable.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.app.savable.domain.member.Member;
 import net.app.savable.domain.member.MemberRepository;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    public Member findMemberById(Long memberId){
-        return memberRepository.findMemberById(memberId)
+    public Member findById(Long memberId){
+        return memberRepository.findById(memberId)
                 .orElseThrow(()-> new IllegalArgumentException("INVALID_MEMBER"+memberId));
     }
 }
