cc6153c854a5190fbf2a60fc5816605000685b0f
DAY0522 <ekdud_____@naver.com>
2023-10-03 03:08:33 +0900

    feat: repository 함수 추가
    - 동일한 이름, 핸드폰 번호를 가진 유저를 찾기 위한 함수

--- src/main/java/net/app/savable/domain/member/MemberRepository.java
+++ src/main/java/net/app/savable/domain/member/MemberRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long>{
     Optional<Member> findMemberById(Long memberId);
     Optional<Member> findByEmail(String email);
+    Optional<Member> findByUsername(String username);
+    Optional<Member> findByPhoneNumber(String phoneNumber);
 }
