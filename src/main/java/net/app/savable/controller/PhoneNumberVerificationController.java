901d6c4bd972a17848452f87860f86fc23f9cee8
DAY0522 <ekdud_____@naver.com>
2023-10-10 07:52:38 +0900

    fix: form-data로 휴대번호를 가져오는 현상 수정
    - json data로 받아오도록 변경함.

--- src/main/java/net/app/savable/controller/PhoneNumberVerificationController.java
+++ src/main/java/net/app/savable/controller/PhoneNumberVerificationController.java
@@ -11,9 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Random;
 
 @Slf4j
@@ -35,14 +37,14 @@ public class PhoneNumberVerificationController {
     }
 
     @PostMapping("/send-sms")
-    public String smsSend(@RequestParam String phoneNumber) {
+    public String smsSend(@RequestBody HashMap<String, String> phoneNumber) {
         log.info("PhoneNumberVerificationController.sendSms() 실행");
 
         Message message = new Message();
         String randomNum = generateRandomNumber();
         // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
         message.setFrom("01082081162");
-        message.setTo(phoneNumber);
+        message.setTo(phoneNumber.get("phoneNumber"));
         message.setText("[Savable] 인증번호는 [" + randomNum + "] 입니다.");
 
         SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
