f8e9ecdb3dcc90257194a50b7d7cefb2a0e0d8c5
DAY0522 <ekdud_____@naver.com>
2023-10-10 16:26:18 +0900

    fix: runtime error 확인을 위한 공통 에러 포맷 처리 비활성화
    - 추후 배포가 된 후에는 수정해야 합니다.

--- src/main/java/net/app/savable/global/error/GlobalExceptionHandler.java
+++ src/main/java/net/app/savable/global/error/GlobalExceptionHandler.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestControllerAdvice(annotations = {RestController.class})
+//@RestControllerAdvice(annotations = {RestController.class})
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
