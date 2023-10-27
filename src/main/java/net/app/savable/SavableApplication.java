15638f4a7a04b347de330ddca4806e354ffc36f1
chae52 <fjdksla98@naver.com>
2023-10-28 02:59:38 +0900

    feat:redis 추가

--- src/main/java/net/app/savable/SavableApplication.java
+++ src/main/java/net/app/savable/SavableApplication.java
@@ -3,6 +3,7 @@ package net.app.savable;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
@@ -13,6 +14,7 @@ import java.util.TimeZone;
 @EnableJpaAuditing
 @EnableScheduling
 @EnableJdbcHttpSession(maxInactiveIntervalInSeconds = 60 * 60 * 24 * 30)
+@EnableCaching
 public class SavableApplication {
 
 	public static void main(String[] args) {
