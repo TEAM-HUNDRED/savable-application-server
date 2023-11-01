efbf08f17ac0d63535f060d548f167d9218eabb3
DAY0522 <ekdud_____@naver.com>
2023-11-01 22:33:52 +0900

    hotfix: 스케줄러 어노테이션 추가

--- src/main/java/net/app/savable/SavableApplication.java
+++ src/main/java/net/app/savable/SavableApplication.java
@@ -4,12 +4,14 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 @EnableJdbcHttpSession(maxInactiveIntervalInSeconds = 60 * 60 * 24 * 30)
 public class SavableApplication {
 
