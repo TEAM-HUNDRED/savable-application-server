5fc176b04041626e2cbe13c407aedf3d84ef998c
chae52 <fjdksla98@naver.com>
2023-11-07 18:32:11 +0900

    feat: MethodArgumentTypeMismatchException 추가

--- src/main/java/net/app/savable/global/error/GlobalExceptionHandler.java
+++ src/main/java/net/app/savable/global/error/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import net.app.savable.global.error.exception.GeneralException;
 import net.app.savable.global.error.exception.InvalidSocialIdException;
 import net.app.savable.global.error.exception.SessionMemberNotFoundException;
 import org.hibernate.exception.ConstraintViolationException;
+
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +16,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice(annotations = {RestController.class})
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
     @ExceptionHandler
     public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
         return handleExceptionInternal(e, ErrorCode.BAD_REQUEST, request);
@@ -47,6 +51,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException e, WebRequest request) {
         return handleExceptionInternal(e, ErrorCode.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> handleTypeMismatchException(MethodArgumentTypeMismatchException e, WebRequest request){
+//        if ("participations".equals(ex.getParameterName())) { // request.url을 보고 해당 API 인지 판별하면 될 듯
+//            // 파라미터 이름이 "participations"와 일치하면 리디렉션
+//            return new RedirectView("/challenges/participations",true);
+//        } else {
+//            // 다른 파라미터에 대한 예외는 다른 처리를 수행하거나 예외 처리를 무시
+////            return new ModelAndView("redirect:/errorPage");
+//        }
+        return handleExceptionInternal(e, ErrorCode.BAD_REQUEST, request);
     }
 
     protected ResponseEntity<Object> handleExceptionInternal(Exception e, Object body,
