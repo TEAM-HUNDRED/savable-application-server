9d57827a05bcb2bc0e7d2e1b275093d78b8b4bf3
DAY0522 <ekdud_____@naver.com>
2023-11-14 05:20:05 +0900

    fix: SessionMemberInterceptor 코드 수정
    - 세션이 없을 시 예외 처리 코드 수정

--- src/main/java/net/app/savable/global/config/interceptor/SessionMemberInterceptor.java
+++ src/main/java/net/app/savable/global/config/interceptor/SessionMemberInterceptor.java
@@ -3,6 +3,7 @@ package net.app.savable.global.config.interceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import net.app.savable.global.config.auth.LoginMember;
 import net.app.savable.global.config.auth.dto.SessionMember;
 import net.app.savable.global.error.exception.SessionMemberNotFoundException;
@@ -11,19 +12,25 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.lang.reflect.Method;
 
+@Slf4j
 public class SessionMemberInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (handler instanceof SessionMemberInterceptor) {
+        log.info("SessionMemberInterceptor.preHandle() 실행");
+        if (handler instanceof HandlerMethod) {
             HandlerMethod handlerMethod = (HandlerMethod) handler;
             Method method = handlerMethod.getMethod();
 
-            if (method.isAnnotationPresent(LoginMember.class)) {
-                HttpSession session = request.getSession();
-                SessionMember member = (SessionMember) session.getAttribute("member");
-                if (member == null) {
+            if (method.isAnnotationPresent(LoginMember.class)) { // @LoginMember 어노테이션이 붙어있는지 확인
+                HttpSession session = request.getSession(false); // 세션이 없으면 null 반환
+                if (session == null) {
                     throw new SessionMemberNotFoundException();
+                } else {
+                    SessionMember member = (SessionMember) session.getAttribute("member");
+                    if (member == null) {
+                        throw new SessionMemberNotFoundException();
+                    }
                 }
             }
         }
