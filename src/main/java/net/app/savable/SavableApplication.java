9bf8d3c8f37fcfcc6fb6ccf0a66dd17e2d81dd6a
chae52 <fjdksla98@naver.com>
2023-11-15 20:11:53 +0900

    test: OOM

--- src/main/java/net/app/savable/SavableApplication.java
+++ src/main/java/net/app/savable/SavableApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
+import java.util.ArrayList;
 import java.util.TimeZone;
 
 @SpringBootApplication
@@ -18,8 +19,19 @@ import java.util.TimeZone;
 public class SavableApplication {
 
 	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		try {
+			for(int i=0; i < 250000; i++) {
+				list.add(new int[10000000]); // 리스트에 배열을 추가한다
+				System.out.println(i);
+				Thread.sleep(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		SpringApplication.run(SavableApplication.class, args);
-	}
+		}
 
 	@PostConstruct
 	void started(){
